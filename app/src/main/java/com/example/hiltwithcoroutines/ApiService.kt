af4160065f1254e6247bf918b41a9cf4d9aa5cd8
Abidujjaman <oviice@gmail.com>
2023-07-24 17:00:33 +0600

    ci/cd

--- app/src/main/java/com/example/hiltwithcoroutines/ApiService.kt
+++ app/src/main/java/com/example/hiltwithcoroutines/ApiService.kt
@@ -1,9 +1,6 @@
 package com.example.hiltwithcoroutines
 
-import com.example.hiltwithcoroutines.model.Response
-import com.example.hiltwithcoroutines.model.ResponseItem
 import com.example.hiltwithcoroutines.model.UserResponseItem
-import okhttp3.ResponseBody
 import retrofit2.http.GET
 
 interface ApiService {
